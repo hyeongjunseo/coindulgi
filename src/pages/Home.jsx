4b948e630d247e827bfd37585924a0775ba4bcce
hyeongjunseo <hjkimseo@gmail.com>
2023-11-28 05:54:21 +0900

    Include quote section in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,24 @@ export default function Home() {
   return (
     <div>
       <Header />
+      <section className="section-quote">
+        <h2>FOLLOW OR BURNT</h2>
+        <blockquote>
+          <p>Imagine how things look from the future</p>
+        </blockquote>
+        <blockquote>
+          <p>The market doesn't care about your profits</p>
+        </blockquote>
+        <blockquote>
+          <p>Milk it while it's available</p>
+        </blockquote>
+        <blockquote>
+          <p>Never bet without your setup</p>
+        </blockquote>
+        <blockquote>
+          <p>Shit happens</p>
+        </blockquote>
+      </section>
     </div>
   );
 }
