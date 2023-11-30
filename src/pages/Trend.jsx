08beac9402776efee66a86f6dd3d6faf735e17f1
hyeongjunseo <hjkimseo@gmail.com>
2023-11-30 12:59:02 +0900

    Implement Trend page with coin list from Redux

--- src/pages/Trend.jsx
+++ src/pages/Trend.jsx
@@ -1,13 +1,33 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchCoins } from "../store";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 export default function Trend() {
   const dispatch = useDispatch();
+  const coins = useSelector((state) => {
+    return state.coins.data;
+  });
+  console.log(coins);
 
   useEffect(() => {
     dispatch(fetchCoins());
   }, [dispatch]);
 
-  return <div>Trend</div>;
+  return (
+    <div>
+      <Header />
+      <section className="section-trend">
+        <h1 className="section-trend-title">TOP-7</h1>
+        <div className="section-trend-list">
+          {coins.map((coin) => (
+            <Link key={coin.id} to={`/coins/${coin.id}`}>
+              <div className="section-trend-coin">{coin.name}</div>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
