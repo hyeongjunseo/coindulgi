8a061c84607f2eda4455c654c32948664bc8c03a
hyeongjunseo <hjkimseo@gmail.com>
2023-12-06 11:28:20 +0900

    Add event list under BTC countdown

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -50,19 +50,51 @@ export default function Event() {
           <div className="event-header-content">
             <div className="event-countdown">
               <div className="event-countdown-number">{countdown.days}</div>
-              <div className="event-countdown-label">D</div>
+              <div>D</div>
             </div>
             <div className="event-countdown">
               <div className="event-countdown-number">{countdown.hours}</div>
-              <div className="event-countdown-label">H</div>
+              <div>H</div>
             </div>
             <div className="event-countdown">
               <div className="event-countdown-number">{countdown.minutes}</div>
-              <div className="event-countdown-label">M</div>
+              <div>M</div>
             </div>
             <div className="event-countdown">
               <div className="event-countdown-number">{countdown.seconds}</div>
-              <div className="event-countdown-label">S</div>
+              <div className="event-countdown-number">S</div>
+            </div>
+          </div>
+        </div>
+        <div className="event-content">
+          <div className="event-sidebar">
+            <ul>
+              <li>Jan 2024</li>
+              <li>Feb 2024</li>
+              <li>Mar 2024</li>
+              <li>Apr 2024</li>
+            </ul>
+          </div>
+          <div className="event-list">
+            <div className="event-section">
+              <div className="event-section-header">
+                <h3>Jan 2024</h3>
+              </div>
+              <div className="event-section-body">
+                <h4 className="event-section-title">Spot BTC ETF</h4>
+                <p className="event-section-description">
+                  Spot BTC ETF approvals ranges between January 5 and 10
+                </p>
+              </div>
+            </div>
+            <div className="event-section">
+              <div className="event-section-header">
+                <h3>Feb 2024</h3>
+              </div>
+              <div className="event-section-body">
+                <h4 className="event-section-title">No events</h4>
+                <p className="event-section-dsecription">No events</p>
+              </div>
             </div>
           </div>
         </div>
