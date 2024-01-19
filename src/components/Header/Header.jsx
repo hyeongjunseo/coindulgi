49928d9ff78ebcf28d1d3d0185e71be92fbdd285
hyeongjunseo <hjkimseo@gmail.com>
2024-01-19 14:59:53 +0900

    Implement responsive design and Add mobile menu bar in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -49,81 +49,154 @@ export default function Header() {
     }
   };
 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   useEffect(() => {
     dispatch(fetchStats());
   }, [dispatch]);
 
   return (
-    <header className="header">
-      <div className="header-stats">
-        <div className="stats-item">
-          <span className="stats-label">Coins:</span>
-          <span className="stats-value">{coins}</span>
+    <div>
+      <header className="header">
+        <div className="header-container">
+          <div className="header-stats">
+            <div className="stats-item">
+              <span className="stats-label">Coins:</span>
+              <span className="stats-value">{coins}</span>
+            </div>
+            <div className="stats-item">
+              Exchanges:
+              <span className="stats-value">{exchanges}</span>
+            </div>
+            <div className="stats-item">
+              <span className="stats-label">Market Cap:</span>
+              <span className="stats-value">${formatNumber(marketCap)}</span>
+              <span
+                className="stats-value-percentage"
+                style={getPercentageStyle(marketCap_percentage)}
+              >
+                {marketCap_percentage >= 0
+                  ? `+${marketCap_percentage}`
+                  : `${marketCap_percentage}`}
+                %
+              </span>
+            </div>
+            <div className="stats-item">
+              <span className="stats-label">24h Vol:</span>
+              <span className="stats-value">${formatNumber(volume)}</span>
+            </div>
+            <div className="stats-item">
+              <span className="stats-label">Dominance:</span>
+              <span className="stats-label">BTC</span>
+              <span className="stats-value">{dominance_btc}%</span>
+              &nbsp;
+              <span className="stats-label">ETH</span>
+              <span className="stats-value">{dominance_eth}%</span>
+            </div>
+          </div>
+          <nav className="header-nav">
+            <ul className="nav-main">
+              <li className="nav-item">
+                <Link className="nav-logo-link" to="/">
+                  <img
+                    className="nav-logo"
+                    src="/dulgi.png"
+                    alt="Coin Dulgi Logo"
+                  />
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="header-menu-item" to="/trend">
+                  TREND
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="header-menu-item" to="/event">
+                  EVENT
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="header-menu-item" to="/news">
+                  NEWS
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="header-menu-item" to="/risk">
+                  RISK 🤿 🌊
+                </Link>
+              </li>
+            </ul>
+            <ul className="nav-external">
+              <li className="nav-item-external">
+                <Link
+                  className="external-link"
+                  to="https://www.binance.com/en"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <img
+                    className="external-logo"
+                    src="/binance.png"
+                    alt="Binance logo"
+                  />
+                </Link>
+              </li>
+              <li className="nav-item-external">
+                <Link
+                  className="external-link"
+                  to="https://www.tradingview.com/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <svg
+                    className="external-logo"
+                    viewBox="0 0 36 28"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M14 22H7V11H0V4h14v18zM28 22h-8l7.5-18h8L28 22z"
+                      fill="currentColor"
+                    ></path>
+                    <circle cx="20" cy="8" r="4" fill="currentColor"></circle>
+                  </svg>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className="stats-item">
-          Exchanges:
-          <span className="stats-value">{exchanges}</span>
-        </div>
-        <div className="stats-item">
-          <span className="stats-label">Market Cap:</span>
-          <span className="stats-value">${formatNumber(marketCap)}</span>
-          <span
-            className="stats-value-percentage"
-            style={getPercentageStyle(marketCap_percentage)}
-          >
-            {marketCap_percentage >= 0
-              ? `+${marketCap_percentage}`
-              : `${marketCap_percentage}`}
-            %
-          </span>
-        </div>
-        <div className="stats-item">
-          <span className="stats-label">24h Vol:</span>
-          <span className="stats-value">${formatNumber(volume)}</span>
-        </div>
-        <div className="stats-item">
-          <span className="stats-label">Dominance:</span>
-          <span className="stats-label">BTC</span>
-          <span className="stats-value">{dominance_btc}%</span>
-          &nbsp;
-          <span className="stats-label">ETH</span>
-          <span className="stats-value">{dominance_eth}%</span>
-        </div>
+      </header>
+      <div className="mobile-menu-bar">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          className="custom-size"
+          onClick={toggleMobileMenu}
+        >
+          <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+        </svg>
       </div>
-      <nav className="header-nav">
-        <ul className="nav-main">
-          <li className="nav-item">
-            <Link className="nav-logo-link" to="/">
-              <img
-                className="nav-logo"
-                src="/dulgi.png"
-                alt="Coin Dulgi Logo"
-              />
-            </Link>
+      <nav className={`mobile-nav ${isMobileMenuOpen ? "open" : ""}`}>
+        <ul className="mobile-nav-list">
+          <li className="mobile-nav-item">
+            <Link to="/">HOME</Link>
           </li>
-          <li className="nav-item">
-            <Link className="header-menu-item" to="/trend">
-              TREND
-            </Link>
+          <li className="mobile-nav-item">
+            <Link to="/trend">TREND</Link>
           </li>
-          <li className="nav-item">
-            <Link className="header-menu-item" to="/event">
-              EVENT
-            </Link>
+          <li className="mobile-nav-item">
+            <Link to="/event">EVENT</Link>
           </li>
-          <li className="nav-item">
-            <Link className="header-menu-item" to="/news">
-              NEWS
-            </Link>
+          <li className="mobile-nav-item">
+            <Link to="/news">NEWS</Link>
           </li>
-          <li className="nav-item">
-            <Link className="header-menu-item" to="/risk">
-              RISK 🤿 🌊
-            </Link>
+          <li className="mobile-nav-item">
+            <Link to="/risk">RISK</Link>
           </li>
-        </ul>
-        <ul className="nav-external">
-          <li className="nav-item-external">
+          <li className="mobile-nav-item mobile-external">
             <Link
               className="external-link"
               to="https://www.binance.com/en"
@@ -136,8 +209,6 @@ export default function Header() {
                 alt="Binance logo"
               />
             </Link>
-          </li>
-          <li className="nav-item-external">
             <Link
               className="external-link"
               to="https://www.tradingview.com/"
@@ -159,6 +230,6 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-    </header>
+    </div>
   );
 }
